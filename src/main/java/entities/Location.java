94e5f3c5e688794e387d009005c25a8349d3bb78
timmy <kris>
2022-05-15 18:19:39 +0200

    .

--- src/main/java/entities/Location.java
+++ src/main/java/entities/Location.java
@@ -11,8 +11,9 @@ import java.util.List;
 @Table(name = "Location")
 public class Location implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private String id;
 
