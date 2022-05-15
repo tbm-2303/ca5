b07a95f3572bf9336e2cec5a5dff8b87a5301d6c
timmy <kris>
2022-05-15 14:15:06 +0200

    :

--- src/main/java/facades/TimelineFacade.java
+++ src/main/java/facades/TimelineFacade.java
@@ -58,7 +58,7 @@ public class TimelineFacade {
             if (timeline == null) {
                 throw new NotFoundException("No timelie with this id exists");
             }
-            Spot spot = new Spot("description");
+            Spot spot = new Spot(spotDTO.getDescription());
             timeline.addSpot(spot);
 
             em.getTransaction().begin();
